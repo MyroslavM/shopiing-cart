a44d4af63577a6857e77d5a14d80547a913ccbc9
MyroslavM <37024405+MyroslavM@users.noreply.github.com>
2019-10-30 12:31:33 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'phone_number' => $faker->numberBetween(10, 30),
         'photo' => Str::random(30),
         'email' => $faker->unique()->safeEmail,
-//        'email_verified_at' => now(),
+
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
